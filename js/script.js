b2234d29c5b6517791810376aef571e20227329d
Ijoelel <fianarnatha13@gmail.com>
2023-06-19 21:53:53 +0700

    update event main system

--- js/script.js
+++ js/script.js
@@ -12,3 +12,75 @@ document.addEventListener("click", function (e) {
 		navbarNav.classList.remove("active");
 	}
 });
+
+fetch("js/event.json")
+	.then((response) => response.json())
+	.then((data) => {
+		const textBoxes = document.querySelectorAll(".text-box");
+		const anchors = document.querySelectorAll(".text-box a");
+
+		const eventDetail = document.querySelector("#event-detail");
+		const eventDetailButtons = document.querySelectorAll(".button");
+		const modalContentText = document.querySelector(".modal-content-text");
+
+		eventDetailButtons.forEach((btn) => {
+			btn.addEventListener("click", (e) => {
+				const eventData = data.event.find((item) => item.id === btn.id);
+				console.log(eventData?.nama);
+				if (eventData) {
+					const namaElement = document.createElement("h3");
+					namaElement.textContent = eventData.nama;
+
+					const tanggalElement = document.createElement("small");
+					tanggalElement.textContent = eventData.tanggal;
+
+					const deskripsiElement = document.createElement("p");
+					deskripsiElement.textContent = eventData.longDesc;
+
+					// untuk mengosongkan modalContentText
+					modalContentText.innerHTML = "";
+
+					// menambahkan h3, small, p element ke modalContentText
+					modalContentText.appendChild(namaElement);
+					modalContentText.appendChild(tanggalElement);
+					modalContentText.appendChild(deskripsiElement);
+				}
+				eventDetail.style.display = "flex";
+				e.preventDefault();
+			});
+		});
+
+		data.event.forEach((item, index) => {
+			const namaElement = document.createElement("h3");
+			namaElement.textContent = item.nama;
+
+			const tanggalElement = document.createElement("small");
+			tanggalElement.textContent = item.tanggal;
+
+			const deskripsiElement = document.createElement("p");
+			deskripsiElement.textContent = item.shortDesc;
+
+			const textBoxIndex = index % textBoxes.length;
+			const anchor = anchors[textBoxIndex];
+
+			textBoxes[textBoxIndex].insertBefore(namaElement, anchor);
+			textBoxes[textBoxIndex].insertBefore(tanggalElement, anchor);
+			textBoxes[textBoxIndex].insertBefore(deskripsiElement, anchor);
+		});
+	});
+
+// klik tombol close
+const eventDetail = document.querySelector("#event-detail");
+
+document.querySelector(".box-modal .close-icon").onclick = (e) => {
+	eventDetail.style.display = "none";
+	e.preventDefault();
+};
+
+// klik diluar box-modal
+const modal = document.querySelector("#event-detail");
+window.onclick = (e) => {
+	if (e.target === modal) {
+		modal.style.display = "none";
+	}
+};
